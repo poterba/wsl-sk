553be1b022634cb077e8da2c6d7c7dde47838c03
woof <vpoterba@luxoft.com>
2019-03-28 17:44:02 +0200

    used universal x-session instead xfce4

--- start.cmd
+++ start.cmd
@@ -3,7 +3,7 @@
 set vbsPath="%tmp%\wslxrun.vbs"
 del %vbsPath%
 
-echo WScript.CreateObject("Shell.Application").ShellExecute "wsl", "DISPLAY=:0 LANG=en_US.UTF-8 startxfce4", "", "open", 0 > %vbsPath%
+echo WScript.CreateObject("Shell.Application").ShellExecute "wsl", "DISPLAY=:0 LANG=en_US.UTF-8 x-session-manager", "", "open", 0 > %vbsPath%
 
 start config.xlaunch 
 wscript %vbsPath%
