f4da5fb01b0878edc2e964d8efe48ce1b92d4de2
vptrbv <vptrbv@gmail.com>
2019-06-25 15:24:20 +0300

    Start WSL in home directory

--- start.cmd
+++ start.cmd
@@ -1,9 +1,11 @@
 @echo off
 
-set vbsPath="%tmp%\wslxrun.vbs"
-del %vbsPath%
+set vbsPath="%TEMP%\wslxrun.vbs"
+del %vbsPath% 2>NUL
 
-echo WScript.CreateObject("Shell.Application").ShellExecute "wsl", "DISPLAY=:0 LANG=en_US.UTF-8 x-window-manager", "", "open", 0 > %vbsPath%
+for /f "usebackq delims=" %%i in (`wsl wslpath -w ~`) do set "BASE_PATH=%%i"
+
+echo WScript.CreateObject("Shell.Application").ShellExecute "wsl", "DISPLAY=:0 LANG=en_US.UTF-8 x-window-manager", "%BASE_PATH%", "open", 0 > %vbsPath%
 
 start .xlaunch 
 wscript %vbsPath%
