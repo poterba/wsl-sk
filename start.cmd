f2934fd0796a745b721a5c213b9b038a467d5bf9
vptrbv <vptrbv@gmail.com>
2019-07-04 15:35:06 +0300

    Autodetect of x-*-manager to start

--- start.cmd
+++ start.cmd
@@ -3,9 +3,11 @@
 set vbsPath="%TEMP%\wslxrun.vbs"
 del %vbsPath% 2>NUL
 
-for /f "usebackq delims=" %%i in (`wsl wslpath -w ~`) do set "BASE_PATH=%%i"
+for /f "delims=" %%i in ('wsl which x-session-manager') do set "COMMAND=%%i"
+if [%COMMAND%] == [] for /f "delims=" %%i in ('wsl which x-window-manager') do set "COMMAND=%%i"
 
-echo WScript.CreateObject("Shell.Application").ShellExecute "wsl", "DISPLAY=:0 LANG=en_US.UTF-8 x-window-manager", "%BASE_PATH%", "open", 0 > %vbsPath%
+for /f "usebackq delims=" %%i in (`wsl wslpath -w ~`) do set "BASE_PATH=%%i"
+echo WScript.CreateObject("Shell.Application").ShellExecute "wsl", "DISPLAY=:0 LANG=en_US.UTF-8 %COMMAND%", "%BASE_PATH%", "open", 0 > %vbsPath%
 
 start .xlaunch 
 wscript %vbsPath%
